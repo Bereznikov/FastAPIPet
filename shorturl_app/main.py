f15c5ab6811187d2501fef80ea91115f6b4c32a4
aleksey_bereznikov <albe@smena.space>
2025-01-07 07:40:39 +0300

    Исправлены докерфайлы в соответствии с архитектурой Исправлена ошибка при решении путей для get_all_links

--- shorturl_app/main.py
+++ shorturl_app/main.py
@@ -1,7 +1,10 @@
 import string
 import random
+
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import BaseModel, HttpUrl
 from sqlalchemy.orm import Session
 from database import SessionLocal, Base, engine
@@ -10,6 +13,19 @@ from models import URLItem
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8001",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class URLCreate(BaseModel):
@@ -48,25 +64,6 @@ def shorten_url(item: URLCreate, db: Session = Depends(get_db)):
     raise HTTPException(status_code=500, detail="Не удалось сгенерировать короткую ссылку")
 
 
-@app.get("/{short_id}")
-def redirect_to_full(short_id: str, db: Session = Depends(get_db)):
-    url_item = db.query(URLItem).filter(URLItem.short_id == short_id).first()
-    if not url_item:
-        raise HTTPException(status_code=404, detail="Короткая ссылка не найдена")
-    return RedirectResponse(url=url_item.full_url)
-
-
-@app.get("/stats/{short_id}")
-def get_stats(short_id: str, db: Session = Depends(get_db)):
-    url_item = db.query(URLItem).filter(URLItem.short_id == short_id).first()
-    if not url_item:
-        raise HTTPException(status_code=404, detail="Короткая ссылка не найдена")
-    return {
-        "short_id": url_item.short_id,
-        "full_url": url_item.full_url
-    }
-
-
 @app.get("/all")
 def get_all_links(db: Session = Depends(get_db)):
     """Получение всех сокращенных ссылок"""
@@ -78,6 +75,27 @@ def get_all_links(db: Session = Depends(get_db)):
         }
         for item in urls
     ]
+
+
+@app.get("/{short_id}")
+def redirect_to_full(short_id: str, db: Session = Depends(get_db)):
+    """Перенаправление на полную ссылку"""
+    url_item = db.query(URLItem).filter(URLItem.short_id == short_id).first()
+    if not url_item:
+        raise HTTPException(status_code=404, detail="Короткая ссылка не найдена")
+    return RedirectResponse(url=url_item.full_url)
+
+
+@app.get("/stats/{short_id}")
+def get_stats(short_id: str, db: Session = Depends(get_db)):
+    """Получение информации об объекте сокращенных ссылок"""
+    url_item = db.query(URLItem).filter(URLItem.short_id == short_id).first()
+    if not url_item:
+        raise HTTPException(status_code=404, detail="Короткая ссылка не найдена")
+    return {
+        "short_id": url_item.short_id,
+        "full_url": url_item.full_url
+    }
 
 
 @app.put("/{short_id}")
